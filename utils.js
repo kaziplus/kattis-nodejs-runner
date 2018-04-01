acc0b1ef24a720f32ac8975ae8dcafcb150f9717
Anthony Nandaa <profnandaa@gmail.com>
2018-04-01 22:25:33 +0300

    feat: main utility script

---
+++ utils.js
@@ -0,0 +1,34 @@
+/**
+ * Read and Write to file (interface)
+ */
+const fs = require('fs');
+
+const inFile = './in-file.txt';
+const outFile = './out-file.txt';
+
+// recreate outfile for idempotency
+try {
+  fs.unlinkSync(outFile);
+} catch (e) {
+  // pass
+}
+
+const inFileContent = fs.readFileSync(inFile, 'utf8');
+const inLines = inFileContent.split('\n');
+
+let lc = 0;
+
+const readline = () => {
+  lc += 1;
+  return inLines[lc -1];
+};
+
+const print = (line) => {
+  // write out line to out-file
+  fs.appendFileSync(outFile, `${line}\n`);
+};
+
+module.exports = {
+   readline,
+   print,
+ };
